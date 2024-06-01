396f8fde630f28a7f5815bb012afc19e83af3a1c
Paulo <paulohenriquemoreirafalcao@gmail.com>
2024-06-01 15:08:35 -0300

    chore: change env variables names

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{os.getenv('username')}:{os.getenv('password')}@localhost:3306/rottentomatoes"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{os.getenv('API_USERNAME')}:{os.getenv('API_PASSWORD')}@{os.getenv('API_DB_URL')}:3306/rottentomatoes"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
